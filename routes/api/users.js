f3bfd9e60153d98ac9d47c1005fb009bcf285c47
Daniel Tovar <daniel@danieltovar.io>
2019-06-12 21:09:04 -0600

    set up JWT and POST api/users returns token

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check'); // ! use express-validate to handle validation and responses 
 const User = require('../../models/User')
 
@@ -55,8 +57,15 @@ router.post('/', [
       await user.save();
   
       // * Return jsonwebtoken
-      console.log(user);
-      res.send('User registered')
+      const payload = { user: { id: user.id } }
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+      })
 
     } catch(err) {
       console.error(err.message);
