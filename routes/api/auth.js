87acca000e253b099bad45b6c05482ea7b445c77
Daniel Tovar <daniel@danieltovar.io>
2019-06-12 22:15:47 -0600

    Authenticated GET api/auth route created

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User')
 
 // * @route   GET api/auth
 // * @desc    Test route
 // * @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+
+  try {
+
+    const user = await User.findById(req.user.id).select('-password'); // select method '-password' leaves off password
+    res.json(user);
+
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server error');
+  }
+
+});
 
 module.exports = router;
