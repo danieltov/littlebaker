6b74352dbbe8050bc9f37a29ee4dfae8c6e6c8e8
Daniel Tovar <daniel@danieltovar.io>
2019-06-18 18:23:40 -0600

    Added accountType to jwt token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,92 +1,87 @@
 // * ==================== DEPENDENCIES ==================== *//
-const express = require( 'express' );
+const express = require('express');
 const router = express.Router();
-const jwt = require( 'jsonwebtoken' );
-const config = require( 'config' );
-const bcrypt = require( 'bcryptjs' );
-const { check, validationResult } = require( 'express-validator/check' ); // ! use express-validate to handle validation and responses 
-const auth = require( '../../middleware/auth' );
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator/check'); // ! use express-validate to handle validation and responses
+const auth = require('../../middleware/auth');
 
 // * ==================== MODEL ==================== * //
-const User = require( '../../models/User' )
-
+const User = require('../../models/User');
 
 // * ==================== ROUTES ==================== *//
 
 // * @route   GET api/auth
 // * @desc    Protected route that returns user data
 // * @access  Public
-router.get( '/', auth, async ( req, res ) => {
-
+router.get('/', auth, async (req, res) => {
   try {
-
-    const user = await User.findById( req.user.id ).select( '-password' ); // select method '-password' leaves off password
-    res.json( user );
-
-  } catch ( err ) {
-    console.err( err.message );
-    res.status( 500 ).send( 'Server error' );
+    const user = await User.findById(req.user.id).select('-password'); // select method '-password' leaves off password
+    res.json(user);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server error');
   }
-
-} );
-
+});
 
 // * @route   POST api/auth
 // * @desc    Authenticate user & get token
 // * @access  Public
-router.post( '/', [
-
-  // ! express-validate functions to validate request body START
-  check( 'email', 'Please enter a valid email address' ).isEmail(),
-  check( 'password', 'Password is required' ).exists(),
-  // ! express-validate functions to validate request body END
-], async ( req, res ) => {
-
-  // ! express-validate error catching START
-  const errors = validationResult( req );
-  if ( !errors.isEmpty() ) {
-    return res.status( 400 ).json( { errors: errors.array() } );
-  }
-  // ! express-validate error catching END
-
-  const { email, password } = req.body;
-
-  try {
-    // * See if user exists
-    let user = await User.findOne( { email } );
-
-    if ( !user ) {
-      return res.status( 400 ).json( { errors: [{ msg: 'Invalid credentials' }] } );
+router.post(
+  '/',
+  [
+    // ! express-validate functions to validate request body START
+    check('email', 'Please enter a valid email address').isEmail(),
+    check('password', 'Password is required').exists()
+    // ! express-validate functions to validate request body END
+  ],
+  async (req, res) => {
+    // ! express-validate error catching START
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+    // ! express-validate error catching END
 
-    // * Compare password
-    const isMatched = await bcrypt.compare( password, user.password );
+    const { email, password } = req.body;
 
-    if ( !isMatched ) {
-      return res.status( 400 ).json( { errors: [{ msg: 'Invalid credentials' }] } );
+    try {
+      // * See if user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      // * Compare password
+      const isMatched = await bcrypt.compare(password, user.password);
+
+      if (!isMatched) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      // * Return jsonwebtoken
+      const payload = { user: { id: user.id, accountType: user.accountType } };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
     }
-
-
-    // * Return jsonwebtoken
-    const payload = { user: { id: user.id } }
-
-    jwt.sign(
-      payload,
-      config.get( 'jwtSecret' ),
-      { expiresIn: 360000 },
-      ( err, token ) => {
-        if ( err ) throw err;
-        res.json( { token } );
-      } )
-
-  } catch ( err ) {
-    console.error( err.message );
-    res.status( 500 ).send( 'Server error' );
   }
-
-
-
-} );
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,82 +1,89 @@
 // * ==================== DEPENDENCIES ==================== *//
-const express = require( 'express' );
+const express = require('express');
 const router = express.Router();
-const gravatar = require( 'gravatar' );
-const bcrypt = require( 'bcryptjs' );
-const jwt = require( 'jsonwebtoken' );
-const config = require( 'config' );
-const { check, validationResult } = require( 'express-validator/check' ); // ! use express-validate to handle validation and responses 
-const User = require( '../../models/User' )
-
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check'); // ! use express-validate to handle validation and responses
+const User = require('../../models/User');
 
 // * ==================== ROUTES ==================== *//
 
 // * @route   POST api/users
 // * @desc    Register user
 // * @access  Public
-router.post( '/', [
-
-  // ! express-validate functions to validate request body START
-  check( 'name', 'Please enter your full name' ).not().isEmpty(),
-  check( 'email', 'Please enter a valid email address' ).isEmail(),
-  check( 'password', 'Please enter a password with 8 or more characters' ).isLength( { min: 8 } ),
-  check( 'accountType', 'Please specify an Account Type' ).not().isEmpty()
-  // ! express-validate functions to validate request body END
-], async ( req, res ) => {
-
-  // ! express-validate error catching START
-  const errors = validationResult( req );
-  if ( !errors.isEmpty() ) {
-    return res.status( 400 ).json( { errors: errors.array() } );
-  }
-  // ! express-validate error catching END
-
-  const { name, email, password, accountType } = req.body;
-
-  try {
-    // * See if user exists
-    let user = await User.findOne( { email } );
-
-    if ( user ) {
-      return res.status( 400 ).json( { errors: [{ msg: 'User already exists' }] } )
+router.post(
+  '/',
+  [
+    // ! express-validate functions to validate request body START
+    check('name', 'Please enter your full name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email address').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 8 or more characters'
+    ).isLength({ min: 8 }),
+    check('accountType', 'Please specify an Account Type')
+      .not()
+      .isEmpty()
+    // ! express-validate functions to validate request body END
+  ],
+  async (req, res) => {
+    // ! express-validate error catching START
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+    // ! express-validate error catching END
 
-    // * Get users gravatar
-    const avatar = gravatar.url( email, {
-      s: '200',
-      r: 'pg',
-      d: 'mm'
-    } );
+    const { name, email, password, accountType } = req.body;
 
-    // * Create an instance of the user
-    user = new User( { name, email, avatar, password, accountType } )
+    try {
+      // * See if user exists
+      let user = await User.findOne({ email });
 
-    // * Encrypt password with bcrypt
-    const salt = await bcrypt.genSalt( 10 );
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
 
-    user.password = await bcrypt.hash( password, salt );
+      // * Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
 
-    // * Save user to database
-    await user.save();
+      // * Create an instance of the user
+      user = new User({ name, email, avatar, password, accountType });
 
-    // * Return jsonwebtoken
-    const payload = { user: { id: user.id } }
-    jwt.sign(
-      payload,
-      config.get( 'jwtSecret' ),
-      { expiresIn: 360000 },
-      ( err, token ) => {
-        if ( err ) throw err;
-        res.json( { token } );
-      } )
+      // * Encrypt password with bcrypt
+      const salt = await bcrypt.genSalt(10);
 
-  } catch ( err ) {
-    console.error( err.message );
-    res.status( 500 ).send( 'Server error' );
+      user.password = await bcrypt.hash(password, salt);
+
+      // * Save user to database
+      await user.save();
+
+      // * Return jsonwebtoken
+      const payload = { user: { id: user.id, accountType: user.accountType } };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
-
-
-
-} );
+);
 
 module.exports = router;
