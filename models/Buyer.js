cfd9e1f06999a435ccd96367992d720746a561ff
Daniel Tovar <daniel@danieltovar.io>
2019-06-22 12:26:15 -0600

    Setting initializers in Buyer and Seller models for products and orders

--- models/Buyer.js
+++ models/Buyer.js
@@ -1,13 +1,15 @@
-const mongoose = require( 'mongoose' );
-const Base = require( './BaseProfile' );
+const mongoose = require('mongoose');
+const Base = require('./BaseProfile');
 
-const BuyerProfileSchema = new mongoose.Schema( {
-  orders: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'product'
-  }
-} );
+const BuyerProfileSchema = new mongoose.Schema({
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'product'
+    }
+  ]
+});
 
-const Buyer = Base.discriminator( 'buyer', BuyerProfileSchema );
+const Buyer = Base.discriminator('buyer', BuyerProfileSchema);
 
-module.exports = mongoose.model( 'buyer' );
+module.exports = mongoose.model('buyer');
--- models/Seller.js
+++ models/Seller.js
@@ -1,13 +1,15 @@
-const mongoose = require( 'mongoose' );
-const Base = require( './BaseProfile' );
+const mongoose = require('mongoose');
+const Base = require('./BaseProfile');
 
-const SellerProfileSchema = new mongoose.Schema( {
-  products: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'product'
-  }
-} );
+const SellerProfileSchema = new mongoose.Schema({
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'product'
+    }
+  ]
+});
 
-const Seller = Base.discriminator( 'seller', SellerProfileSchema );
+const Seller = Base.discriminator('seller', SellerProfileSchema);
 
-module.exports = mongoose.model( 'seller' );
+module.exports = mongoose.model('seller');
