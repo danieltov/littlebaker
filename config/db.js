e4ec355e0b9f182ac91fb4b43402e57316af479b
Daniel Tovar <daniel@danieltovar.io>
2019-06-22 11:04:48 -0600

    useFindAndModify depreciation warning fix

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ const db = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
 
     console.log('MongoDB Connected.');
   } catch (err) {
